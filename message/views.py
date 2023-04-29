73c2c9cd6c135b4bad244aa7a41d0602d8679f17
vionaosiako <viona.osiako@student.moringaschool.com>
2023-04-29 22:14:12 +0300

    Source recepiengt from the user table

--- message/views.py
+++ message/views.py
@@ -1,14 +1,18 @@
-    
-from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 import africastalking
 import requests
 from django.conf import settings
 import os
+from .models import *
+from .serializers import *
+from authentication.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import permissions
 
 def send_sms(request):
-# Set your app credentials
+    # Set your app credentials
     username = os.environ.get('USER1')
     api_key = os.environ.get('API_KEY')
     
@@ -23,18 +27,21 @@ def send_sms(request):
     sms = africastalking.SMS
 
     # Set the numbers you want to send to in international format
-    recipients = ["+254703616854"]#"+254768852080"
+    phoneList=[]
+    recipients = User.objects.all()
+    for recipient in recipients:
+    #"+254768852080"
 
     # Set your message
-    message = "Hello. Regards TheLioness"
+        message = request.data['message']
 
-    # Set your shortCode or senderId
-    # sender = "shortCode or senderId"
-    try:
-        # Thats it, hit send and we'll take care of the rest.
-        response = sms.send(message, recipients)
-        print("am response sms", response)
-        return HttpResponse(response)
-    except Exception as e:
-        return HttpResponse('Encountered an error while sending: %s' % str(e))
+        # Set your shortCode or senderId
+        # sender = "shortCode or senderId"
+        try:
+            # Thats it, hit send and we'll take care of the rest.
+            response = sms.send(message, ["+254{}".format(recipient.phone_number[1:])])
+            print("am response sms", response)
+            return HttpResponse(response)
+        except Exception as e:
+            return HttpResponse('Encountered an error while sending: %s' % str(e))
 
