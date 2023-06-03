df3e6cb30b46f2434ee81be7a1aed92b03a57865
vionaosiako <viona.osiako@student.moringaschool.com>
2023-06-03 18:13:22 +0300

    clean the code

--- loans/views.py
+++ loans/views.py
@@ -142,49 +142,6 @@ def getLoanPaymentDetails(request,id):
 #-------------------------------------------------------------------------------------------------------------------------------------
 #Loan approved
 #-------------------------------------------------------------------------------------------------------------------------------------
-# @api_view(['GET','PUT','DELETE'])
-# def approved_request(request, id):
-#     speficLoanRequest = LoanRequest.objects.get(pk=id)
-    
-#     today = date.today()
-#     status_date = today.strftime("%B %d, %Y")
-#     speficLoanRequest.status_date = status_date
-#     speficLoanRequest.save()
-#     year = speficLoanRequest.payment_period_years
-
-#     approved_user = LoanRequest.objects.get(id=id).user
-#     if CustomerLoan.objects.filter(user=approved_user).exists():
-
-#         # find previous amount of user
-#         PreviousAmount = CustomerLoan.objects.get(
-#             user=approved_user).total_loan
-#         PreviousPayable = CustomerLoan.objects.get(
-#             user=approved_user).payable_loan
-
-#         # update balance
-#         CustomerLoan.objects.filter(
-#             user=approved_user).update(total_loan=int(PreviousAmount)+int(loan_obj.amount_requested))
-#         CustomerLoan.objects.filter(
-#             user=approved_user).update(payable_loan=int(PreviousPayable)+int(loan_obj.amount_requested)+int(loan_obj.amount_requested)*0.12*int(year))
-
-#     else:
-
-#         # request user
-
-#         # CustomerLoan object create
-#         save_loan = CustomerLoan()
-
-#         save_loan.customer = approved_user
-#         save_loan.total_loan = int(speficLoanRequest.amount_requested)
-#         save_loan.payable_loan = int(
-#             speficLoanRequest.amount_requested)+int(speficLoanRequest.amount_requested)*0.12*int(year)
-#         save_loan.save()
-
-#     loanRequest.objects.filter(id=id).update(status='approved')
-#     loanrequest = loanRequest.objects.filter(status='pending')
-#     # return render(request, 'admin/request_user.html', context={'loanrequest': loanrequest})
-#     return JsonResponse(context={'loanrequest': loanrequest})
-
 @api_view (['GET'])
 def getApprovedLoans(request):
     if request.method == 'GET':
@@ -198,19 +155,6 @@ def getApprovedLoans(request):
 #-------------------------------------------------------------------------------------------------------------------------------------
 #Loan disapproved
 #-------------------------------------------------------------------------------------------------------------------------------------
-
-# def rejected_request(request, id):
-
-#     today = date.today()
-#     status_date = today.strftime("%B %d, %Y")
-#     loan_obj = loanRequest.objects.get(id=id)
-#     loan_obj.status_date = status_date
-#     loan_obj.save()
-#     # rejected_customer = loanRequest.objects.get(id=id).customer
-#     # print(rejected_customer)
-#     loanRequest.objects.filter(id=id).update(status='rejected')
-#     loanrequest = loanRequest.objects.filter(status='pending')
-#     return render(request, 'admin/request_user.html', context={'loanrequest': loanrequest})
 @api_view (['GET'])
 def getRejectedLoans(request):
     if request.method == 'GET':
